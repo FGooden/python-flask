cab772c02ea40e6a56fe136cc894788fcc785ccb
FGooden <86357251+FGooden@users.noreply.github.com>
2022-12-15 09:45:19 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,34 @@ terraform {
     }
   }
 
-  #required_version = ">= 1.0.0"
+  #required_version = ">= 1.2.0"
 }
 
 provider "aws" {
+  profile = "default"
   region = "eu-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-0648ea225c13e0729"
+resource "aws_instance" "dockeros" {
+  ami           = "ami-0648ea225c13e0729" #Amazon Linux 2 AMI
   instance_type = "t2.micro"
   
   #keypair
   key_name = "dockerkey"
 
   tags = {
-    Name = "Jenkinstf"
+    Name = "DockJenk"
+  }
+  
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = aws_instance.dockeros.public_ip
   }
 }
+
+output "Instance_ip" {
+  value = aws_instance.dockeros.public_ip
+}
+  
+
