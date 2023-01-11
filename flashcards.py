b84d47e8b583c409e8cf68328d1676aa41ca12f8
FGooden <86357251+FGooden@users.noreply.github.com>
2023-01-11 16:56:21 +0000

    Update flashcards.py

--- flashcards.py
+++ flashcards.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def welcome():
     return "Welcome to my Flash Cards with Docker."
     
-if __name__ == "__main__":
-app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+app.run(debug=True, host="0.0.0.0", port=5000)
